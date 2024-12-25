eead35c9f8bfcf56f30523c134533eeda72f38b6
Ingo Macherius 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 <ingo@macherius.com>
2024-12-25 06:29:48 +0100

    Java language modernize without change

--- src/main/java/net/sf/repbot/RepBot.java
+++ src/main/java/net/sf/repbot/RepBot.java
@@ -1,10 +1,11 @@
 package net.sf.repbot;
 
 import javax.inject.Inject;
+
 import net.sf.repbot.command.NullCommand;
-import net.sf.repbot.dagger.RepbotComponent;
 import net.sf.repbot.dagger.DaggerRepbotComponent;
 import net.sf.repbot.dagger.RepBotModule;
+import net.sf.repbot.dagger.RepbotComponent;
 import net.sf.repbot.db.UserDB;
 import net.sf.repbot.db.UserDBException;
 import net.sf.repbot.preferences.Preferences;
@@ -12,7 +13,6 @@ import net.sf.repbot.preferences.PreferencesImpl;
 import net.sf.repbot.server.FibsListener;
 import net.sf.repbot.server.Monitor;
 import net.sf.repbot.services.CommandDispatcher;
-import net.sf.repbot.services.ConnectionActivity;
 import net.sf.repbot.services.Executor;
 import net.sf.repbot.services.Logger;
 import net.sf.repbot.services.MatchEventListener;
